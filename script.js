bad69f782856da2f69a617b60b722074c9638430
vinay kumar <150110499+vinay630987@users.noreply.github.com>
2024-03-07 22:00:22 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".social-list").addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
